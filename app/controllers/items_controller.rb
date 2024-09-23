de3b8b3f64b06689500f99db27a0fb02093b01d2
daiki0527 <linkindaiki0527@gmail.com>
2024-09-23 22:29:29 +0900

    新規登録からログアウトまで

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,13 @@ class ItemsController < ApplicationController
   def index
     @items = Item.includes(:user)
   end
+
+  def new
+    @items = Item.new
+  end
+
+  def create
+    @items = Item.new(prototype_params)
+    redirect_to '/'
+  end
 end
