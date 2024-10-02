c9ecbd04165264263d7436f95ca988de639380b2
daiki0527 <linkindaiki0527@gmail.com>
2024-10-02 21:12:54 +0900

    ユーザー登録

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,7 @@ class ApplicationController < ActionController::Base
   end
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :email, :encrypted_password, :last_name, :first_name])
+    devise_parameter_sanitizer.permit(:sign_up,
+                                      keys: [:nickname, :last_name, :first_name, :birth, :first_name_kana, :last_name_kana])
   end
 end
