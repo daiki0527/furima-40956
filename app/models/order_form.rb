2d557b65ce750fa6838a385c59dcecac0e4dcdd8
daiki0527 <linkindaiki0527@gmail.com>
2024-10-17 03:43:11 +0900

    formオブジェクト

---
+++ app/models/order_form.rb
@@ -0,0 +1,20 @@
+class OrderForm
+  include ActiveModel::Model
+  attr_accessor :token, :user_id, :item_id, :post_code, :prefecture_id, :city, :address, :building, :telephone_number
+
+  with_options presence: true do
+    validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
+    validates :telephone_number, format: { with: /\Ad{10}$|^\d{11}\z/, message: 'is invalid.' }
+    validates :user_id
+    validates :item_id
+    validates :city
+    validates :address
+    validates :token
+  end
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+
+  def save
+    order_form = OrderForm.create(user_id:, item_id:)
+    Sending.create(post_code: post_code, prefecture_id: prefecture_id, city: city, address: address, building: building, telephone_number: telephone_number)
+  end
+end
