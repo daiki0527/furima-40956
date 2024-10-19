14fa6fdfb2e129964fdfbbefa33682603245840f
daiki0527 <linkindaiki0527@gmail.com>
2024-10-19 20:42:41 +0900

    ストロングパラメーター①

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,16 @@
 class OrdersController < ApplicationController
+  before_action :set_order, only: [:index, :create]
   def index
     @order_form = OrderForm.new
+  end
+
+  def create
+    @order_form = OrderForm.new(order_params)
+  end
+
+  private
+
+  def set_order
+    @item = Item.find(params[:item_id])
   end
 end
